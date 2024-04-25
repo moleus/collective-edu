375fb79b341666f5f7496d68dbe2e20985116fd6
Moleus <fafufuburr@gmail.com>
2024-04-26 00:18:08 +0300

    mvp

--- frontend/background.js
+++ frontend/background.js
@@ -1,65 +1,56 @@
-// Function to intercept requests
+const openeduProblemCheckUrls = ["https://courses.openedu.ru/courses/*/problem_check"];
+
 const interceptRequest = async (details) => {
-  console.log("Request URL: " + details.url);
+    console.log("Request URL: " + details.url);
 };
 
-// Function to intercept responses
-const interceptResponse = async (details) => {
-  const response = await fetch(details.url);
-  const responseBody = await response.text();
-  console.log("Response body: " + responseBody);
-};
-
-// Add listener to intercept requests
-// browser.webRequest.onBeforeRequest.addListener(
-//   interceptRequest,
-//   { urls: ["https://courses.openedu.ru/courses/*/problem_check"] },
-//   ["requestBody"]
-// );
 browser.webRequest.onBeforeRequest.addListener(
-  interceptRequest,
-  { urls: ["https://apps.openedu.ru/*"] },
-  ["requestBody"]
+    interceptRequest,
+    { urls: openeduProblemCheckUrls }
 );
 
 // Add listener to intercept responses
 browser.webRequest.onCompleted.addListener(
-  interceptResponse,
-  { urls: ["https://courses.openedu.ru/courses/*/problem_check"] },
-  ["responseBody"]
+    interceptResponse,
+    { urls: openeduProblemCheckUrls }
 );
 
-function logURL(requestDetails) {
-  console.log(`Loading: ${requestDetails.url}`);
+function interceptResponse(details) {
+    console.log("Response URL: " + details.url);
+    console.log("Response status: " + details.statusCode);
 }
 
-browser.webRequest.onBeforeRequest.addListener(logURL, {
-  urls: ["<all_urls>"],
-});
+let filter = browser.webRequest.filterResponseData(details.requestId);
+filter.ondata = (event) => {
+    console.log(`filter.ondata received ${event.data.byteLength} bytes`);
+    filter.write(event.data);
+};
+filter.onstop = (event) => {
+    // The extension should always call filter.close() or filter.disconnect()
+    // after creating the StreamFilter, otherwise the response is kept alive forever.
+    // If processing of the response data is finished, use close. If any remaining
+    // response data should be processed by Firefox, use disconnect.
+    filter.close();
+};
 
+function listener(details) {
+    let filter = browser.webRequest.filterResponseData(details.requestId);
+    let decoder = new TextDecoder("utf-8");
+    let encoder = new TextEncoder();
 
-// // match pattern for the URLs to redirect
-// let pattern = "https://developer.mozilla.org/*";
+    filter.ondata = (event) => {
+        let response = decoder.decode(event.data, { stream: true });
+        console.log(`Response content: ${response}`);
+        filter.write(encoder.encode(response));
+        filter.disconnect();
+    };
 
-// // URL we will redirect to
-// let redirectUrl =
-//   "https://38.media.tumblr.com/tumblr_ldbj01lZiP1qe0eclo1_500.gif";
+    return {};
+}
 
-// // redirect function returns a Promise
-// // which is resolved with the redirect URL when a timer expires
-// function redirectAsync(requestDetails) {
-//   console.log(`Redirecting async: ${requestDetails.url}`);
-//   return new Promise((resolve, reject) => {
-//     setTimeout(() => {
-//       resolve({ redirectUrl });
-//     }, 2000);
-//   });
-// }
+browser.webRequest.onBeforeRequest.addListener(
+    listener,
+    { urls: openeduProblemCheckUrls, types: ["main_frame"] },
+    ["blocking"],
+);
 
-// // add the listener,
-// // passing the filter argument and "blocking"
-// browser.webRequest.onBeforeRequest.addListener(
-//   redirectAsync,
-//   { urls: [pattern], types: ["image"] },
-//   ["blocking"],
-// );
