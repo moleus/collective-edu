c04691bb7efb3e6c17aee8c4b50c7604d66b4615
Moleus <fafufuburr@gmail.com>
2024-04-26 00:39:04 +0300

    remove old

--- frontend/old/popup.js
+++
@@ -1,5 +0,0 @@
-// Display intercepted data in popup
-chrome.runtime.onMessage.addListener(function(message, sender, sendResponse) {
-  document.getElementById('content').textContent = message.data;
-});
-
