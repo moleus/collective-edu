1b29e005879b59756260a9eb9c5a842d06a39d4f
Pavel Soloviev <66182149+Moleus@users.noreply.github.com>
2024-05-01 22:04:56 +0300

    feat: process choice tasks; don't duplicate; only one correct (#12)
    * feat: process choice tasks; don't duplicate; only one correct
    
    * refactor(front): less permissions
    
    * chore(front): update server ip; tune docker-compose
    
    * refactor: remove unused code; allow remote domain connection
    
    * add volume for redis

--- frontend/src/checker/QuestionStorage.ts
+++ frontend/src/checker/QuestionStorage.ts
@@ -1,38 +1,13 @@
 import {ProcessedQuestionAnswer} from "./AllDataProcessor";
 
-const SOLUTIONS_PATH = "http://127.0.0.1:8080/solutions";
+const SOLUTIONS_PATH = "http://185.112.102.15:8080/solutions";
 
 export interface QuestionStorage {
     save(...answers: ProcessedQuestionAnswer[]): Promise<void>;
 
     get(id: QuestionId): Promise<ProcessedQuestionAnswer[]>;
-}
 
-export class LocalStorage implements QuestionStorage {
-    private readonly answers: ProcessedQuestionAnswer[] = []
-    private localStorageKey: string = 'openEduAnswers'; // Key to store answers in local storage
-
-    constructor() {
-        const storedAnswers = localStorage.getItem(this.localStorageKey);
-        if (storedAnswers) {
-            this.answers = JSON.parse(storedAnswers);
-        }
-    }
-
-    save(answers: ProcessedQuestionAnswer): Promise<void> {
-        console.log("Saving new answers");
-        this.answers.push(answers);
-        this.saveToLocalStorage();
-        return Promise.resolve();
-    }
-
-    async get(id: string): Promise<ProcessedQuestionAnswer[]> {
-        return this.answers.filter((e) => e.taskId == id);
-    }
-
-    private saveToLocalStorage(): void {
-        localStorage.setItem(this.localStorageKey, JSON.stringify(this.answers));
-    }
+    getMany(ids: QuestionId[]): Promise<ProcessedQuestionAnswer[][]>
 }
 
 export class ServerStorage implements QuestionStorage {
@@ -52,3 +27,12 @@ export class ServerStorage implements QuestionStorage {
         console.log("Getting answers from server");
         return fetch(SOLUTIONS_PATH + `?taskIDs=${id}`).then((r) => r.json()).catch((e) => console.error(e))
     }
+
+    async getMany(ids: string[]): Promise<ProcessedQuestionAnswer[][]> {
+        console.log("Getting answers from server for multiple ids");
+        const idsParam = ids.join(',');
+        return fetch(SOLUTIONS_PATH + `?taskIDs=${idsParam}`)
+            .then((r) => r.json())
+            .catch((e) => console.error(e));
+    }
+}
